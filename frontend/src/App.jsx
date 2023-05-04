126bd84359c3583ca787665e550c0ee9b333cb7f
Pasindu Kushan <https://github.com/it21110016>
2023-05-04 15:56:33 +0530

    added candidate login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import {BrowserRouter,Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
+import CandidateLogin from './pages/CandidateLogin'
+import AdministratorLogin from './pages/AdministratorLogin'
+import ElectionResults from './pages/ElectionResults'
 
 function App() {
 
@@ -8,6 +11,9 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='/CandidateLogin' element={<CandidateLogin />}/>
+        <Route path='/AdministratorLogin' element={<AdministratorLogin />}/>
+        <Route path='/Electionresults' element={<ElectionResults />}/>
       </Routes>
       </BrowserRouter>
     </>
